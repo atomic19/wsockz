2369d8bd13a33150d6e9ceef4557e4f3d8e2e952
atomic19 <bofh1920@gmail.com>
2020-08-16 03:53:35 -0700

    updateS

---
+++ file/index.js
@@ -0,0 +1,147 @@
+;(() => {
+    // expectingMessage is set to true
+    // if the user has just submitted a message
+    // and so we should scroll the next message into view when received.
+    let expectingMessage = false
+
+    function uuidv4() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }      
+
+    function dial() {
+        namele = document.getElementById("name")
+        rndidele = document.getElementById("rndID")
+
+        if(namele.value == null || namele.value.length == 0){
+            appendLog("enter name to connect", true)
+            return;
+        }
+
+        document.getElementById("rndID").value = uuidv4().toString()
+        document.getElementById("connect").disabled = true
+        namele.disabled = true
+
+      const conn = new WebSocket(`ws://${location.host}/register?rndID=${rndidele.value}&info=&name=${namele.value}`)
+  
+      conn.addEventListener("close", ev => {
+        appendLog(`WebSocket Disconnected code: ${ev.code}, reason: ${ev.reason}`, true)
+        document.getElementById("connect").disabled = false
+        document.getElementById("name").disabled = false
+      })
+      conn.addEventListener("open", ev => {
+        console.info("websocket connected")
+        appendLog("websocket connected")
+        getList()
+      })
+  
+      // This is where we handle messages received.
+      conn.addEventListener("message", ev => {
+        if (typeof ev.data !== "string") {
+          console.error("unexpected message type", typeof ev.data)
+          return
+        }
+        const p = appendLog(ev.data)
+        if (expectingMessage) {
+          p.scrollIntoView()
+          expectingMessage = false
+        }
+      })
+    }
+    document.getElementById("connect").onclick = dial
+  
+    const messageLog = document.getElementById("message-log")
+    const messageInput = document.getElementById("message-input")
+  
+    // appendLog appends the passed text to messageLog.
+    function appendLog(text, error) {
+      const p = document.createElement("p")
+      p.style = "float: left; width: 100%"
+      // Adding a timestamp to each message makes the log easier to read.
+      p.innerText = `${new Date().toLocaleTimeString()}: ${text}`
+      if (error) {
+        p.style.color = "red"
+        p.style.fontStyle = "bold"
+      }
+      messageLog.append(p)
+      return p
+    }
+    appendLog("type name and click connect")
+
+    function createElementFromHTML(htmlString) {
+        var div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+      
+        // Change this to div.childNodes to support multiple top-level nodes
+        return div.firstChild; 
+      }
+
+    const getList = async () => {
+        try{
+            const resp = await fetch("/list", { method: "GET"})
+            if (resp.status != 200){
+                throw new Error(`failed to get response ${resp.status} ${resp.message}`)
+            }
+            const online = await resp.json()
+            all_online = online
+            htmlDivs = []
+
+            Object.keys(online).forEach(function (item) {
+                const e1 = `<div style="float:left; width: 100px">
+                <label style="float: left;">${online[item].Name}</label>
+                <button style="float: right" id="callAudBtn" value="${item}">call</button>
+                </div>`;
+                htmlDivs.push(e1)
+            });
+
+
+            document.getElementById("online-users").innerHTML = htmlDivs.join()
+
+           // debugger;
+        }
+        catch(err){
+            appendLog(`failed to get online list wait for 30 seconds to retry or reload page  error: ${err}`, true)
+        }
+        setTimeout(getList, 30*1000)
+    }
+
+    all_online = {}
+    const wrapper = document.getElementById('online-users');
+    
+    wrapper.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
+
+        console.dir(event.target.value);
+        callAudFn(event.target.value)
+    })
+  
+    // onsubmit publishes the message from the user when the form is submitted.
+    callAudFn = async id => {
+        appendLog(`calling user ${all_online[id].Name}`)
+  
+    //   const msg = messageInput.value
+    //   if (msg === "") {
+    //     return
+    //   }
+    //   messageInput.value = ""
+  
+    //   expectingMessage = true
+    //   try {
+    //     const resp = await fetch("/publish", {
+    //       method: "POST",
+    //       body: msg,
+    //     })
+    //     if (resp.status !== 202) {
+    //       throw new Error(`Unexpected HTTP Status ${resp.status} ${resp.statusText}`)
+    //     }
+    //   } catch (err) {
+    //     appendLog(`Publish failed: ${err.message}`, true)
+    //   }
+
+    }
+  })()
+  
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/atomic19/wsockz
+
+go 1.14
+
+require (
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
+	nhooyr.io/websocket v1.8.6
+)
